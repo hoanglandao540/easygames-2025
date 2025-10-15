021089918337e5d3ca40d6351a06c5528e2192ec
Hoang Lan Dao <hoanglandao540@gmail.com>
2025-10-16 09:24:08 +1000

    Revert "feat(guest-flow): HomeController Index shows Guest when anonymous; role-based redirect when authenticated"
    This reverts commit 10422ab0556a56612f718018a46c03fd3b76a797.

--- src/EasyGames.Web/EasyGames.Web/Controllers/HomeController.cs
+++ src/EasyGames.Web/EasyGames.Web/Controllers/HomeController.cs
@@ -1,33 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 
 namespace EasyGames.Web.Controllers
 {
+    // student-style: simple landing page
     public class HomeController : Controller
     {
-        // GET: /
-        // If not logged in => show Guest view
-        // If logged in => redirect by role
-        public IActionResult Index()
-        {
-            if (!(User?.Identity?.IsAuthenticated ?? false))
-            {
-                // not logged in => show guest landing
-                return View("Guest");
-            }
-
-            // logged in => route by role
-            var role = User.FindFirst(ClaimTypes.Role)?.Value ?? "";
-            return role switch
-            {
-                "Owner" => RedirectToAction("Index", "Products", new { area = "Owner" }),
-                "Shop" => RedirectToAction("Index", "Pos", new { area = "Shop" }),
-                "Customer" => RedirectToAction("Index", "Catalog", new { area = "Storefront" }),
-                _ => RedirectToAction("Index", "Catalog", new { area = "Storefront" }) // default
-            };
-        }
-
-        // Optional explicit route: /Home/Guest
-        public IActionResult Guest() => View();
+        public IActionResult Index() => View();
     }
 }
